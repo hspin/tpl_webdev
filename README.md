fc0088c37d102a9ace8d2dde1b045eb230faa9dd
GS CloudVM <me.gsbit@hsx5.com>
2016-07-05 15:27:19 -0400

    first initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# full stack web development starter template
+
+## client - frontend development
+
+npm run setup - installs bower and npm modules
+
+npm run wire - wire up front end dependencies
+
+npm run start - watch live development server
+
+npm run build - build static final files
+
+npm run serve - webserver see build static files
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var usemin = require('gulp-jade-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var rev = require('gulp-rev');
+var rename = require("gulp-rename");
+
+gulp.task('usemin', function() {
+  gulp.src('./client/jade/_layout_working.jade')
+    .pipe(usemin({
+      css: ['concat'],
+      html: [minifyHtml({empty: true})],
+      js: [uglify()]
+    }))
+    .pipe(gulp.dest('./client/preview'));
+
+  gulp.src("./client/preview/_layout_working.jade")
+    .pipe(rename("layout.jade"))
+    .pipe(gulp.dest("./client/jade/"));
+
+});
+
+gulp.task('rename-mv', function() {
+  gulp.src("./client/preview/_layout_working.jade")
+  .pipe(rename("layout.jade"))
+  .pipe(gulp.dest("./client/jade/"));
+});
+
+      // js: [uglify(), rev()]
